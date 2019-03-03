ac1d0f8544b7758dc4bbe9d8128e341839e23d26
Tolstoyevsky <guy.smoilov@gmail.com>
2019-03-03 19:50:29 +0200

    Make gdrive_download portable to dash (the default shell in colab)

--- data/gdrive-download.sh
+++ data/gdrive-download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-function gdrive_download () { 
+gdrive_download () {
   CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$1" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')
   wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
   rm -f /tmp/cookies.txt
